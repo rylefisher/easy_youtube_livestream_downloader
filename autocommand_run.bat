4e5a6c717deb1312a10085dcab64aebd672413b8
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-04-27 03:34:57 -0400

    Update autocommand_run.bat

--- autocommand_run.bat
+++ autocommand_run.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p video_url="Enter YouTube Video URL: "
-ytarchive --threads 3 -w -k -t --vp9 --merge --no-frag-files %video_url% best
+ytarchive --threads 6 -w -k -t --vp9 --merge --no-frag-files %video_url% best
 pause
