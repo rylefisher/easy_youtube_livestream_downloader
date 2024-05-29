caa5aaa9d725d2724249e83a53b2400d3b3edd45
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-05-29 19:10:19 -0400

    Add files via upload

--- autocommand_run.bat
+++ autocommand_run.bat
@@ -1,4 +1,4 @@
 @echo off
-set /p video_url="Enter YouTube Video URL: "
+set /p video_url="CTRL-C once to abruptly end and save. Enter YouTube Livestream Video URL: "
 ytarchive --threads 6 -w -k -t --vp9 --merge --no-frag-files %video_url% best
 pause
