11806bd3d94b5d952f0253343ac2d5199e40a2e4
Sam A <98753696+rylefisher@users.noreply.github.com>
2025-04-07 09:55:42 -0500

    Delete LIVESTREAMDOWNLOADER.bat

--- LIVESTREAMDOWNLOADER.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: Update yt-dlp to the latest version
-yt-dlp -U
-yt-dlp --live-from-start -N 64 --cookies "C:\Users\dower\OneDrive\Desktop\cookies.txt"  https://www.youtube.com/live/PzyEJe5vn74?si=YJgBxxNMnWkV6okL  -o "%outputFile%"
-pause
